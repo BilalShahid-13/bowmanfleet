c2d9e3ed9e4ebd5ec68b8b68257ae3c6c83d021f
Bilal Shahid <bcsm-f21-249@superior.edu.pk>
2026-05-22 17:38:00 +0500

    update next config

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,12 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   output: 'export',
   trailingSlash: true,
+  assetPrefix: './',
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
+//  <link rel="stylesheet" href="./_next/static/chunks/0md2k86kyut7..css" data-precedence="next" />
+//     <link rel="stylesheet" href="./_next/static/chunks/0mec6oqg9m0ct.css" data-precedence="next" />
